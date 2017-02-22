98ce7a5623268c9ff2d79be91865dad960cc09d5
duraimurugan <durai145@live.in>
2017-02-21 23:13:23 -0500

    first commit

---
+++ haraka-outbound/outmail.sh
@@ -0,0 +1,3 @@
+#swaks -f youremail@yourdomain.com -t test@heaerieglobalsolutions.com -s localhost \
+#  -p 587 -au user1 -ap india
+swaks -h haraka.test -f dxxxx@gmail.com -t admin@heaerieglobalsolutions.com -s localhost -p 25 -au user1 -ap password1
